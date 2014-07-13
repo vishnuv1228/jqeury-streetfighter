faf6481461844569d6c101fdd9c873fb221a2281
vishnuv1228 <vishnuv1228@gmail.com>
2014-07-13 15:25:06 +0800

    Hadouken Sound and Animation

--- js/app.js
+++ js/app.js
@@ -7,15 +7,28 @@ $(document).ready(function() {
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
 	})
-	.mousedown(function() {
-		// play hadouken sound
-		$('.ryu-ready').hide();
-		$('.ryu-throwing').show();
-		$('.hadouken').show();
-		// show hadouken and animate it to the right of the screen
-	})
+    .mousedown(function() {
+    	playHadouken();
+    	$('.ryu-ready').hide();  
+    	$('.ryu-throwing').show();
+    	$('.hadouken').finish().show()
+      	.animate(
+        	{'left': '800px'},
+        	500,
+        	function() {
+          		$(this).hide();
+          		$(this).css('left', '520px');
+        	}
+     	 );
+     })
 	.mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	});
 });
+
+function playHadouken () {
+  	$('#hadouken-sound')[0].volume = 0.5;
+  	$('#hadouken-sound')[0].load();
+ 	$('#hadouken-sound')[0].play();
+}	
